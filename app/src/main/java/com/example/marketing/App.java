934c6dddd78d0358b5172c3248281df83cb06d64
Sergey Sazonov <sergey.s.sazonov@gmail.com>
2024-10-07 13:30:50 +0300

    Стиль кодирования

--- app/src/main/java/com/example/marketing/App.java
+++ app/src/main/java/com/example/marketing/App.java
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 public class App
 {
-    public static void main(String[] args)
-    {
-        SpringApplicationUtils.runWithBufferingApplicationStartup(args, App.class);
-    }
+	public static void main(String[] args)
+	{
+		SpringApplicationUtils.runWithBufferingApplicationStartup(args, App.class);
+	}
 }
