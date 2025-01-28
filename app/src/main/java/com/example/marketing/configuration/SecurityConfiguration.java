57c35c55336b92abed47d022e956044def14dfc7
Sergey Sazonov <sergey.s.sazonov@gmail.com>
2025-01-28 15:49:18 +0300

    Добавили стандартную авторизацию Spring Boot

--- app/src/main/java/com/example/marketing/configuration/SecurityConfiguration.java
+++ app/src/main/java/com/example/marketing/configuration/SecurityConfiguration.java
@@ -5,7 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,8 +21,24 @@ public class SecurityConfiguration
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception
 	{
-		return http.authorizeHttpRequests(configurer -> configurer.anyRequest().permitAll())
-				.csrf(AbstractHttpConfigurer::disable)
+		return http
+				.authorizeHttpRequests(requests -> requests
+						.anyRequest().authenticated())
+				.formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+				.logout(LogoutConfigurer::permitAll)
+				.csrf(CsrfConfigurer::disable)
 				.build();
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService()
+	{
+		var user = User.withDefaultPasswordEncoder()
+				.username("admin")
+				.password("admin")
+				.roles("USER")
+				.build();
+
+		return new InMemoryUserDetailsManager(user);
 	}
 }
